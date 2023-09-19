784344bfbb4ad8513e036f83f58f509fe2cdc736
Haseeb Ahmed <107324392+Halmoside@users.noreply.github.com>
2023-09-20 01:44:46 +0530

    Add files via upload

---
+++ Javascript By Hitesh Sir/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict" // --> Treat all Js code as newer version
+
+ // alert(3 + 3) --> We use Nodejs, not browser
+
+ console.log(3 
+    + 
+    3)  // --> Code readibility should be high.
+
+
+    // Datatypes...
+
+    let name = "Haseeb"  // --> String
+    let age = 21          // --> Number
+    let isLoggedIn = false     // --> Boolean
+
+
+
+    // Number  =>  45
+    // null    => Standalone value   --> Null is a object
+    // BigInt  =>   234556789875567887n
+    // Boolean  => true / false
+    // string   => "Haseeb"
+    // Symbol   =>  Which is unique
+    // undefined   =>  Which is not defined
+
+
+    // --> "typeof" Operator tells the type
+
+
+
+
