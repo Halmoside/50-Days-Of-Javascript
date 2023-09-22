6507c63978865e635b0d11e4ad608bb1630346be
Haseeb Ahmed <107324392+Halmoside@users.noreply.github.com>
2023-09-22 23:35:24 +0530

    Add files via upload

---
+++ Javascript By Hitesh Sir/04_comparisions.js
@@ -0,0 +1,29 @@
+
+// ---------------------------------------------------------------
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+// ------------------------------------------------------------------
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+// --------------------------------------------------------------------
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// -----------------------------------------------------------------------
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === --> Known as 'Strict Check'
+
+console.log("2" == 2);
+console.log("2" === 2); // Strick Check
